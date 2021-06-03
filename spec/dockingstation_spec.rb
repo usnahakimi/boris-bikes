a892d5eadb2ff09970c322b2bc6bfa127a777728
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-06-03 13:34:18 +0100

    first

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,8 +1,10 @@
 require './lib/dockingstation.rb'
 
 describe DockingStation do
-    it 'return true if an instance of DockingStation responds to release_bike' do
-        docking_station = DockingStation.new
-        expect(docking_station.respond_to?(:release_bike)).to eq(true)
+    it { is_expected.to respond_to :release_bike }
+
+    it 'releases working bikes' do
+        bike = subject.release_bike
+        expect(bike).to be_working
     end
 end
